54b675fc850c2067f3c787683ec71791023a3b7a
a-creative <m.andersen.post@gmail.com>
2022-09-17 18:04:49 +0200

    Dev utils

---
+++ backend/src/fs_prod.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo chown maya:maya -R storage
+sudo chown maya:maya -R bootstrap
---
+++ backend/src/fs_www.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo chown www-root:www-root -R storage
+sudo chown www-root:www-root -R bootstrap
