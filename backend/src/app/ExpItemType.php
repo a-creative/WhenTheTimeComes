d6e8f75fa2c94dcdea4012192f072823486649bb
a-creative <m.andersen.post@gmail.com>
2022-09-17 17:22:05 +0200

    Init laravel model for *ItemType

---
+++ backend/src/app/ExpItemType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExpItemType extends Model
+{
+    use HasFactory;
+}
---
+++ backend/src/app/Http/Controllers/ExpItemTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ExpItemType;
+use Illuminate\Http\Request;
+
+class ExpItemTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ExpItemType  $expItemType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ExpItemType $expItemType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ExpItemType  $expItemType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ExpItemType $expItemType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ExpItemType  $expItemType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ExpItemType $expItemType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ExpItemType  $expItemType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ExpItemType $expItemType)
+    {
+        //
+    }
+}
