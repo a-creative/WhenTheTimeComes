5d5e034302d521c8162df68fee439bb6684776ff
a-creative <m.andersen.post@gmail.com>
2022-09-24 15:43:51 +0200

    Wrong group/user for www user

--- backend/src/fs_www.sh
+++ backend/src/fs_www.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo chown www-root:www-root -R storage
-sudo chown www-root:www-root -R bootstrap
+sudo chown www-data:www-data -R storage
+sudo chown www-data:www-data -R bootstrap
