fe39785140aa0c49cf633f0d8c60b3b094f7ab98
a-creative <m.andersen.post@gmail.com>
2022-10-02 20:15:17 +0200

    Controllers init.

---
+++ backend/src/app/Http/Controllers/ActualAmountController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ActualAmount;
+use Illuminate\Http\Request;
+
+class ActualAmountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ActualAmount  $actualAmount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ActualAmount $actualAmount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ActualAmount  $actualAmount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ActualAmount $actualAmount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ActualAmount  $actualAmount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ActualAmount $actualAmount)
+    {
+        //
+    }
+}
---
+++ backend/src/app/Http/Controllers/ItemTypePlanController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ItemTypePlan;
+use Illuminate\Http\Request;
+
+class ItemTypePlanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ItemTypePlan  $itemTypePlan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ItemTypePlan $itemTypePlan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ItemTypePlan  $itemTypePlan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ItemTypePlan $itemTypePlan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ItemTypePlan  $itemTypePlan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ItemTypePlan $itemTypePlan)
+    {
+        //
+    }
+}
